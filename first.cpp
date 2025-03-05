72efc10430aa1203c02a773acbdff1253c750d0b
TSX97 <zachem08@icloud.com>
2025-03-05 19:09:26 +0000

    сделал этому п****скому коду нормальную ориентацию (обьектную)

--- first.cpp
+++ first.cpp
@@ -1,18 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int factorial(unsigned n){
-	if(n == 1){
-		return 1;
+class Number{
+
+public:
+
+	int num;
+
+	int factorial(int n){
+		if(n == 1){
+			return 1;
+		}
+		else if(n > 1){
+			return n * factorial(n-1);
+		} else return 0;
 	}
-	else if(n > 1){
-		return n * factorial(n-1);
-	} else return 0;
-}
+	Number(int p_num){
+		num = p_num;
+	}
+
+
+};
+
+
 
 int main(){
+
+	Number x = (5);
+
 	for(int i = 0; i < 11; i++){
-		cout << factorial(i + 1) << "\n";
+		cout << x.factorial(i + 1) << "\n";
 	}
 	return 0;
 }
