fdbafb2fdce3f84e6d5319a36ee526d545319e6a
TSX97 <zachem08@icloud.com>
2025-03-11 20:14:29 +0000

    доделал. дело было в константной функции принт

--- scrs/virtual2.cpp
+++ scrs/virtual2.cpp
@@ -1,96 +1,114 @@
-#include <iostream> //доработать
+#include <iostream>
 #include <string>
 using namespace std;
 
-
-class Person{
- 
-
- private:
-  int age;
-  string name;
-  
- public:
-  Person(int age, string name): age(age), name(name) {}
-  
-  int getAge(){return age;}
-  
-  string getName(){return name;}
-  
-  virtual void print() const {
-   cout << "name: " << name << " age: " << age << endl;
-  }
-};
+void sep(){ cout << endl; }
 
 
+class Person {
 
-class Programmer: public Person{
- 
 
- private:
-  int exp;
-  string lang;
-  
- public:
-  
-  Programmer(int age, string name, int exp, string lang): Person(age, name), exp(exp), lang(lang) {}
-  
-  
- 
-  int getExp(){return exp;}
-  
-  string getLang(){return lang;}
-  
-  virtual void print() override const {
-   cout << "name: " << getName() << " age: " << getAge() << " exp: " << exp << " lang: " << lang << endl;
-  }
-  
-  
+private:
+    int age;
+    string name;
+
+
+public:
+    Person(int age, string name) : age(age), name(name) {}
+
+    int getAge() { return age; }
+
+    string getName() { return name; }
+
+    void setAge(int age) { this->age = age; }
+
+    virtual void print() {
+        cout << "name: " << name << " age: " << age << endl;
+    }
 };
 
 
 
 
-class Junior: public Programmer{
- 
- private:
- 
-  string drc; //direction - направление
- 
- public:
-  
-  Junior(int age, string name, int exp, string lang, string drc): Person(age, name), Programmer(exp, lang), drc(drc) {}
-  
-  void print(){
-   cout << "name: " << getName() << " age: " << getAge() << " exp: " << getExp() << " lang: " << getLang() <<  " direction: " << drc << endl;
-  }
-  
-  string getDrc(){return drc;}
-  
-  void askStupidQuestion(){
-   cout << "fuck brain" << endl;
-  }
-  
 
-  
-  
-  
-  
-  
+class Programmer : public Person {
+
+
+private:
+    int exp;
+    string lang;
+
+
+public:
+    Programmer(int age, string name, int exp, string lang) : Person(age, name), exp(exp), lang(lang) {}
+
+    int getExp() { return exp; }
+
+    string getLang() { return lang; }
+
+    void setExp(int exp){ this->exp = exp; }
+
+    void setLang(string lang){ this->lang = lang; }
+
+    virtual void print() override {
+        cout << "name: " << getName() << " age: " << getAge() << " exp: " << exp << " lang: " << lang << endl;
+    }
 };
 
 
 
-int main(){
- Person person = {17, "sereja"};
- person.print();
- 
- Programmer programmer = {16, "pasha", 2, "C++"};
- programmer.print();
- 
- Junior jun = {19, "matvey"}
- 
- 
- 
- return 0;
+
+class Junior : public Programmer {
+
+
+private:
+    string drc; // direction - направление
+
+
+
+public:
+    Junior(int age, string name, int exp, string lang, string drc) : Programmer(age, name, exp, lang), drc(drc) {}
+
+    void print() override {
+        cout << "name: " << getName() << " age: " << getAge() << " exp: " << getExp() << " lang: " << getLang() << " direction: " << drc << endl;
+    }
+
+    string getDrc() { return drc; }
+
+    void setDrc(string drc){ this->drc = drc;}
+
+    void askStupidQuestion() {
+        cout << getName() << " fuck brain" << endl; //оьй блять не могу к приватным полям родительского класса без геттеров обратиться
+    }    // извините забыл у нас же в стране инкапсуляция
+    
+};
+
+int main() {
+    Person person1(17, "sereja"); 
+    person1.print();
+    person1.setAge(18);
+    cout << "happybirthday\n";
+    person1.print();
+
+    sep();
+    
+    Programmer programmer(16, "pasha", 2, "C++"); 
+    programmer.print();
+    programmer.setAge(17);
+    cout << "happybirthday\n";
+    programmer.print();
+    
+sep();
+
+    Junior jun(19, "matvey", 1, "Python", "Web");
+    jun.print(); 
+    jun.askStupidQuestion();
+
+    Person person2 = {static_cast<Person>(person1)};
+    person2.print(); // ээээээээээээээ надо дописать конструктор копирования в классах + деструктор желательно
+    return 0;
 }
+
+
+
+//ебал я в рот IDE на телефоне X(
