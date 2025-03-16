dc2bf6cc7ba646e6b04ce84d75055784eafd26a8
TSX97 <zachem08@icloud.com>
2025-03-16 17:03:15 +0000

    дописал класс дробей

--- scrs/rational.cpp
+++ scrs/rational.cpp
@@ -76,6 +76,20 @@ class Rational{
             }
         }
 
+        Rational operator* (Rational other){
+            return {this->num * other.num, this->den * other.den};
+        }
+
+        Rational operator/ (Rational other){
+            swap(this->num, this->den);
+            return {this->num * other.num, this->den * other.den};
+        }
+        
+        Rational operator% (Rational other){
+            swap(this->num, this->den);
+            cerr << "долбаеб";
+        }
+
         string operator[](string index) const{
             if(index == "num"){
                 return to_string(num);
@@ -83,17 +97,16 @@ class Rational{
                 return to_string(den);
             } else {
                 return "Invalid index";
-            }
-
-            
-             
-             
-                 
-            
+            }            
         }
     
 
         operator int() const { return num / den; }
+
+        // operator double() const {
+        //     double result = num  den;
+        //     return result;
+        // }
 
         operator bool() const { return num != 0;}
 
@@ -103,20 +116,19 @@ class Rational{
 
 
 int main(){
-
+    setlocale(LC_ALL, "RU");
     Rational rat = {1, 2};
     rat.print();
 
-    cout << "-\n";
+    cout << "*\n";
 
     Rational rat2 = {1, 4};
     rat2.print();
 
     cout << "=\n";
 
-    Rational ratres = {rat - rat2};
+    Rational ratres = {rat *  rat2};
     ratres.print();
-
 
     
     return 0;
