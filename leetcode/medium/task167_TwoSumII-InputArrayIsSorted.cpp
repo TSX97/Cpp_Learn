66c5320fee21efd0db37b0553458309a354b786e
TSX97 <zachem08@icloud.com>
2025-03-27 22:17:31 +0000

    я изначально думал в эту сторону, но потом передумал и все пошло по пизде

--- leetcode/medium/task167_TwoSumII-InputArrayIsSorted.cpp
+++ leetcode/medium/task167_TwoSumII-InputArrayIsSorted.cpp
@@ -1,21 +1,16 @@
-#include <cmath>
-
-class Solution { //алгоритм подходящий, но превышает время исполнения, пройдено 23/24 тестов(
-//вернусь к ней когда изучу map 
-public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        const int size = numbers.size();
-        vector<int> result;
-        for(int i = 0; i < size; i++){
-            for(int j = i; j < size; j++){
-
-                if(numbers[i]+numbers[j] == target && i != j){
-                    result.push_back(i+1);
-                    result.push_back(j+1);
-                    return result;
+class Solution {
+    public:
+        vector<int> twoSum(vector<int>& numbers, int target) { //блять это решалось ебаным бинарным поиском
+            int l = 0, r = numbers.size() - 1;
+            while (r > l) {
+                if (numbers[r] + numbers[l] == target) {
+                    return {l + 1, r + 1};
+                } else if (numbers[r] + numbers[l] > target) {
+                    r--;
+                } else {
+                    l++;
                 }
             }
+            return {-1, -1};
         }
-    return {1, 2};    
-    }
-}; 
+    };
