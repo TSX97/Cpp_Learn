1b892fab601853023642484ad122b21aeb2e9e37
TSX97 <zachem08@icloud.com>
2025-04-28 19:04:37 +0500

    мб напишу мини-язык свой на костылях

---
+++ C/a.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define beg {
+#define end }
+
+#define u unsgined
+
+#define i2 short
+#define i4 int
+#define i8 long 
+
+#define f4 float
+#define d8 double
+#define dd long double
+
+#define c char
+
+#define v void
+
+
+i4 main() 
+beg
+
+		c word[] = "hello";
+		
+		printf("%s", word);
+
+		return 0;
+
+end
