9b635c095d1517729dccbca525ee11e1025f71a5
TSX97 <zachem08@icloud.com>
2025-05-27 13:12:18 +0300

    Update d2.cpp

--- qt/d2.cpp
+++ qt/d2.cpp
@@ -4,12 +4,19 @@
 #include <QLabel>
 #include <QPushButton>
 #include <QVBoxLayout>
+#include <QHBoxLayout>
 #include <QString>
 
+
 static int count = 0;
-void OnClick();
-QLabel* label;
-QPushButton* button;
+void RangCheck();
+void Plus();
+void Minus();
+QLabel* rang; //ранг отоброжаемый
+QLabel* counter; //счетчик
+QPushButton* BtnPls; //+
+QPushButton* BtnMns; //-
+
 
 int main(int argc, char* argv[]){
 
@@ -21,48 +28,78 @@ int main(int argc, char* argv[]){
     widget->setMinimumHeight(400);
     widget->setMaximumWidth(600);
 
-    label = new QLabel(widget);
-    button = new QPushButton(widget);
+    rang = new QLabel(widget);
+    counter = new QLabel(widget);
+    rang->setText("clicker");
+    counter->setText("0");
 
-    label->setText("clicker");
-    QObject::connect(button, &QPushButton::clicked, OnClick);
+    BtnPls = new QPushButton(widget);
+    BtnMns = new QPushButton(widget);
+    QObject::connect(BtnPls, &QPushButton::clicked, Plus);
+    QObject::connect(BtnMns, &QPushButton::clicked, Minus);
+    BtnPls->setText("+");
+    BtnMns->setText("-");
 
-    QVBoxLayout* vlay = new QVBoxLayout(widget);
+    QHBoxLayout* hlayI = new QHBoxLayout(); // Horizontal Layout Interface
+    QHBoxLayout* hlayC = new QHBoxLayout(); // Horizontal Layout Counter
+    QVBoxLayout* vlay = new QVBoxLayout();
 
-    vlay->addWidget(label);
-    vlay->addWidget(button);
+    hlayI->addWidget(rang);
+    hlayI->addWidget(counter);
+
+    hlayC->addWidget(BtnMns);
+    hlayC->addWidget(BtnPls);
+
+
+
+    vlay->addLayout(hlayI);
+    vlay->addLayout(hlayC);
+    widget->setLayout(vlay);
 
 
     widget->show();
     return a.exec();
+
 }
 
-void OnClick(){
-
-    count++;
-    button->setText(QString::number(count));
+void RangCheck(){
     if(count == 25){
-        label->setText("Herald");
+        rang->setText("Herald");
     }
     else if(count == 50){
-        label->setText("Guardian");
+        rang->setText("Guardian");
     }
     else if(count == 100){
-        label->setText("Crusader");
+        rang->setText("Crusader");
     }
     else if(count == 250){
-        label->setText("Archon");
+        rang->setText("Archon");
     }
     else if(count == 500){
-        label->setText("Legend");
+        rang->setText("Legend");
     }
     else if(count == 1000){
-        label->setText("Anciant");
+        rang->setText("Anciant");
     }
     else if(count == 2500){
-        label->setText("Divine");
+        rang->setText("Divine");
     }
     else if(count == 10000){
-        label->setText("Immortal");
+        rang->setText("Immortal");
     }
 }
+
+void Plus(){
+
+    count++;
+    counter->setText(QString::number(count));
+    RangCheck();
+}
+
+void Minus(){
+
+    count--;
+    counter->setText(QString::number(count));
+    RangCheck();
+}
+
